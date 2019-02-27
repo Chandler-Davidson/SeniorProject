04772bc8be5d2b58b87fc26ed8806ac5dd38f26f
Robert Harvey <rkh0005@uah.edu>
2019-02-27 13:53:31 -0600

    Update Parser.cpp
    Editing parser to pull out words.
    
    Uploading to test on windows computer.

--- SourceCodeChecker/SourceCodeChecker/Parser.cpp
+++ SourceCodeChecker/SourceCodeChecker/Parser.cpp
@@ -18,10 +18,45 @@ void Parser::Parse(string filePath)
 	ifstream myfile;
 	myfile.open(filePath);
 	string line;
+	size_t found = NULL;
+	size_t previous = 0;
+	string word = "";
+	const string searchStr = " ";
+
 	if(myfile.is_open())
 	{
 		while(getline(myfile,line))
 		{
+			//Check the line for "words" 
+			//by getting all data between spaces
+			do
+			{
+				// Blank word for the next run
+				word = "";
+
+				//Check for the first space
+				found = line.find(searchStr);
+				//Returns string::npos (-1) if end of line
+				if(found != string::npos)
+				{
+					//Check if there is a space at start
+					if(found == 0){
+						//If there is just make that the last previous
+						previous = found;
+					}
+					//As long as it's not end of line pull the "word" out
+					else if(found != string::npos){
+						//pull the "word" out
+						word = line.substr(found, previous);
+						previous = found;
+					}
+
+				}
+
+				if(word != "")
+					cout << word << "\n"; //Send word to rule manager
+
+			}while(found1 != string::npos); // Stop checking if end of line
 			cout << line << '\n';
 		}
 		myfile.close();
