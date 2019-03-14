2dac219fd37875f9cb62ea0dfa8334f630b95b39
Robert Harvey <rkh0005@uah.edu>
2019-03-14 17:30:25 -0500

    Update RuleManager.cpp
    Moved comments into cpp.
    Started functions

--- SourceCodeChecker/SourceCodeChecker/RuleManager.cpp
+++ SourceCodeChecker/SourceCodeChecker/RuleManager.cpp
@@ -5,8 +5,14 @@
 
 #include "RuleManager.h"
 
-// Set to NULL initialy
+// Initialize the RuleManager instance
 RuleManager* RuleManager::instance = new RuleManager();
+
+// Constructior for RuleManager
+RuleManager::RuleManager()
+{
+
+}
 
 // Will return the instance of the RuleManager
 RuleManager* RuleManager::getInstance()
@@ -14,7 +20,23 @@ RuleManager* RuleManager::getInstance()
 	return instance;
 }
 
-// Constructior for RuleManager
-RuleManager::RuleManager()
+// For receiving the active rules. Return false if
+// there is any errors setting up the rules.
+// Create using the rule name?
+bool setActiveRules(vector<string> active)
 {
+
+}
+
+// Used by parser to send the string
+void run(string input)
+{
+
+}
+
+// Will be called at the end of a file and retreive the
+// final metrics from the rules then push them to storage
+void finished()
+{
+
 }
