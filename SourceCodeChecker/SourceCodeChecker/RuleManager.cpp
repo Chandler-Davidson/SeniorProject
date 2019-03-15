e011fbcd46cdcba7a7798fa0a021a88a64115e45
Noyesdude <33379079+Noyesdude@users.noreply.github.com>
2019-03-14 19:14:30 -0500

    Update RuleManager.cpp
    Added code to run and finished

--- SourceCodeChecker/SourceCodeChecker/RuleManager.cpp
+++ SourceCodeChecker/SourceCodeChecker/RuleManager.cpp
@@ -23,20 +23,33 @@ RuleManager* RuleManager::getInstance()
 // For receiving the active rules. Return false if
 // there is any errors setting up the rules.
 // Create using the rule name?
-bool setActiveRules(vector<string> active)
+bool RuleManager::setActiveRules(vector<string> active)
 {
 
 }
 
 // Used by parser to send the string
-void run(string input)
+void RuleManager::run(string input)
 {
+	int i = 0;
 	// Run rules that are active
+	// Send line to each rule
+	// for(int i = 0; i < activeRules.size(); i++)
+	rules[activeRules[i]].run(input);
 }
 
 // Will be called at the end of a file and retreive the
 // final metrics from the rules then push them to storage
-void finished()
+void RuleManager::finished()
 {
+	string data; // Temp string to hold metrics
+
+	// Loop all active rules to tell them the
+	// parser is finished
+	for (int i = 0; i < activeRules.size(); i++)
+	{
+		data = rules[activeRules[i]].finished();
+	}
+
 	// Once finished store metrics
 }
