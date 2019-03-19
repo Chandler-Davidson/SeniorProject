776a77cdf71cbf915bde9bbfd22b1b74dc3cbe12
Noyesdude <33379079+Noyesdude@users.noreply.github.com>
2019-03-19 04:46:36 -0500

    Update RuleManager.cpp
    Fixed singleton implementation.
    Wrote constructor.
    Added temp testing code.
    Started setting up setActiveRules.
    Added setName for filePath.
    Edited run

--- SourceCodeChecker/SourceCodeChecker/RuleManager.cpp
+++ SourceCodeChecker/SourceCodeChecker/RuleManager.cpp
@@ -1,22 +1,48 @@
 /*
-*	Last worked on 3/13/2019
+*	Last worked on 3/19/2019
 *	RuleManager.cpp
 */
 
 #include "RuleManager.h"
 
-// Initialize the RuleManager instance
-RuleManager* RuleManager::instance = new RuleManager();
+RuleManager* RuleManager::instance = 0;
 
 // Constructior for RuleManager
 RuleManager::RuleManager()
 {
+	int i = 0;
+	// Get list of rules on creation
+	for (std::filesystem::directory_entry p : std::filesystem::directory_iterator("Rules"))
+	{
+		string ruleName = p.path().string();
+		int end = ruleName.find(".cpp");
+		if (end != string::npos)
+		{
+			// Start at the end of the directory name
+			// and substract directory name and .cpp
+			// from the total length.
+			ruleName = ruleName.substr(6, ruleName.size() - 10);
+			// Add rule to list of callable rules
+			//rules[i] = callFacotry(ruleName);
+		}
+	}
 
+	static IntCountRule* p = new IntCountRule();
+
+	// Temp till the rule factory can be setup
+	rules.emplace_back(p);
+
+	fileName = "";
 }
 
 // Will return the instance of the RuleManager
 RuleManager* RuleManager::getInstance()
 {
+	if (instance == 0)
+	{
+		instance = new RuleManager();
+	}
+
 	return instance;
 }
 
@@ -25,17 +51,32 @@ RuleManager* RuleManager::getInstance()
 // Create using the rule name?
 bool RuleManager::setActiveRules(vector<string> active)
 {
+	// Check names in given vector with names of
+	// rules found in directory.
+	
+	// As found create an array of active rules
+	// to iterate over.
 
+	//temp for testing
+	activeRules.push_back(0);
+
+	return true;
+}
+
+// For receiving the file name. Return false if
+// there is any errors setting the name.
+void RuleManager::setName(string filePath)
+{
+	fileName = filePath;
 }
 
 // Used by parser to send the string
 void RuleManager::run(string input)
 {
-	int i = 0;
 	// Run rules that are active
 	// Send line to each rule
-	// for(int i = 0; i < activeRules.size(); i++)
-	rules[activeRules[i]].run(input);
+	for(size_t i = 0; i < activeRules.size(); i++)
+		rules[activeRules[i]]->run(input);
 }
 
 // Will be called at the end of a file and retreive the
@@ -46,10 +87,12 @@ void RuleManager::finished()
 
 	// Loop all active rules to tell them the
 	// parser is finished
-	for (int i = 0; i < activeRules.size(); i++)
+	for (size_t i = 0; i < activeRules.size(); ++i)
 	{
-		data = rules[activeRules[i]].finished();
+		data = rules[activeRules[i]]->finished();
+
+		cout << "Stored data for: " << rules[activeRules[i]]->getName() << ".\n";
 	}
 
-	// Once finished store metrics
+	fileName = "";
 }
