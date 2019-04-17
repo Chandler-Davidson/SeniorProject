0ed1e93894a9b4d25d648f415177ab2bb1a1efa4
Noyesdude <33379079+Noyesdude@users.noreply.github.com>
2019-04-16 23:17:37 -0500

    Cleaned comments and tried to fix msbuild issue on my computer

--- Docs/Build.bat
+++ Docs/Build.bat
@@ -1,27 +1,27 @@
-# Install Chocolatey
+:: Install Chocolatey
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-# Install MongoDB
+:: Install MongoDB
 choco install mongodb -y
 
-# Install MsBuild
+:: Install MsBuild
 choco install microsoft-build-tools
 
-# Install nuget
+:: Install nuget
 choco install nuget.commandline
 
-# Install Node.js
+:: Install Node.js
 
-# Build C++ project
-msbuild ..\Src\BackendAnalyzer\SourceCodeChecker.sln
+:: Build C++ project
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" ..\Src\BackendAnalyzer\SourceCodeChecker.sln
 
-# Install C# project dependencies
+:: Install C# project dependencies
 nuget restore ..\Src\Server\SeniorServer.sln
 
-# Build C# project
-msbuild ..\Src\Server\SeniorServer.sln
+:: Build C# project
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" ..\Src\Server\SeniorServer.sln
 
-# Install JS project dependencies
+:: Install JS project dependencies
 npm install ..\Src\Client
 
 pause
