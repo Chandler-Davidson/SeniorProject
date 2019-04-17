af9f72d5b45475e053c8c85bb0a2be8c579bf6ae
Chandler Davidson <chandlermdavidson@gmail.com>
2019-04-17 12:00:24 -0500

    Add environment refresh

--- Docs/Build.bat
+++ Docs/Build.bat
@@ -1,24 +1,46 @@
 :: Install Chocolatey
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-:: Install MongoDB
-choco install mongodb -y
+:: Refresh PATH environment
+:: Get System PATH
+for /f "tokens=2*" %%A in ('reg query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path') do set syspath=%%B
 
-:: Install MsBuild
-choco install microsoft-build-tools
+:: Get User Path
+for /f "tokens=2*" %%A in ('reg query "HKCU\Environment" /v Path') do set userpath=%%B
+
+:: Set Refreshed Path
+set PATH=%userpath%;%syspath%
+
+:: Install MongoDB
+choco upgrade mongodb -y
 
 :: Install nuget
-choco install nuget.commandline
+choco upgrade nuget.commandline -y
 
 :: Install Node.js
+choco upgrade nodejs -y
+
+:: Refresh PATH environment
+:: Get System PATH
+for /f "tokens=2*" %%A in ('reg query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path') do set syspath=%%B
+
+:: Get User Path
+for /f "tokens=2*" %%A in ('reg query "HKCU\Environment" /v Path') do set userpath=%%B
+
+:: Set Refreshed Path
+set PATH=%userpath%;%syspath%
+
+:: Move to local directory
+@setlocal enableextensions
+@cd /d "%~dp0"
 
 :: Build C++ project
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" ..\Src\BackendAnalyzer\SourceCodeChecker.sln
+%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" ..\Src\BackendAnalyzer\SourceCodeChecker.sln
 
-:: Install C# project dependencies
+ :: Install C# project dependencies
 nuget restore ..\Src\Server\SeniorServer.sln
 
-:: Build C# project
+ :: Build C# project
 "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" ..\Src\Server\SeniorServer.sln
 
 :: Install JS project dependencies
