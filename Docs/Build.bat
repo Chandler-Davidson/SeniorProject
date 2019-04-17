12022261bcba48e8a30274120e9c173691b316e6
Chandler Davidson <chandlermdavidson@gmail.com>
2019-04-16 22:40:49 -0500

    Add vs build tools

--- Docs/Build.bat
+++ Docs/Build.bat
@@ -2,15 +2,26 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 # Install MongoDB
-choco install mongodb
+choco install mongodb -y
+
+# Install MsBuild
+choco install microsoft-build-tools
+
+# Install nuget
+choco install nuget.commandline
+
+# Install Node.js
 
 # Build C++ project
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" ..\Src\BackendAnalyzer\SourceCodeChecker.sln
+msbuild ..\Src\BackendAnalyzer\SourceCodeChecker.sln
+
+# Install C# project dependencies
+nuget restore ..\Src\Server\SeniorServer.sln
 
 # Build C# project
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" ..\Src\Server\SeniorServer.sln /t:restore
+msbuild ..\Src\Server\SeniorServer.sln
 
-cd ..\Src\Client
+# Install JS project dependencies
+npm install ..\Src\Client
 
-# Install dependencies for JS project
-npm install
+pause
